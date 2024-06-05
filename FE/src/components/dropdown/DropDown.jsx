4ff8d0e1dd0a322bb67590b3ed04b642b950194d
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-06-06 00:16:27 +0900

    feat : showdropdown hook add

--- FE/src/components/dropdown/DropDown.jsx
+++ FE/src/components/dropdown/DropDown.jsx
@@ -1,13 +1,25 @@
-import DropDownList from "@/components/dropdown/DropDownList"
-import { sortList } from "@/constants/dropdownList"
+import { useRef, useState } from "react";
+import { ReactComponent as ToggleImg } from "@/assets/icons/ic_toggle.svg";
+import TableList from "@/components/table/TableList";
+import { sortList } from "@/constants/dropdownList";
+import useShowDropDown from "@/hooks/useShowDropDown";
+import * as S from "./dropdown.module.css";
 
 //TODO: 사용하는곳에서 sortList를 사용하고 list로 변경해주기
-function DropDown () {
+function DropDown() {
+  const ref = useRef();
+  const [showOptions, setShowOptions] = useShowDropDown(ref, false);
+  const [value, setValue] = useState(sortList[2]);
+
   return (
-    <div>
-      <DropDownList lists={sortList}/> 
+    <div className={S.container}>
+      <div className={S.dropdownContainer} ref={ref} >
+        {value}
+        <ToggleImg onClick={() => setShowOptions(!showOptions)}/>
+      </div>
+      {showOptions && <TableList lists={sortList} setValue={setValue}/>}
     </div>
-  )
+  );
 }
 
-export default DropDown
+export default DropDown;
