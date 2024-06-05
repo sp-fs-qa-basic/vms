4cf87d5f6ef901d3c6c34c486e9055951d2f44ab
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-06-05 18:02:23 +0900

    boilerplate

---
+++ FE/src/components/search/Search.jsx
@@ -0,0 +1,11 @@
+import * as S from "./search.module.css";
+
+function Search() {
+  return (
+    <div className={S.container}>
+      <input />
+    </div>
+  );
+}
+
+export default Search;
---
+++ FE/src/pages/CompaniesPage.jsx
@@ -0,0 +1,5 @@
+function CompaniesPage () {
+
+}
+
+export default CompaniesPage
