3fa59529831eabad97d916c83497fbc2c0088f94
Soeun Kim <summerkim0825@gmail.com>
2024-06-09 23:02:30 +0900

    Feat: 오류 처리

--- BE/app.js
+++ BE/app.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import * as dotenv from "dotenv";
 import express from "express";
 dotenv.config();
@@ -8,55 +8,83 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-app.get("/companies", async (req, res) => {
-  const {
-    offset = 0,
-    limit = 10,
-    view = "accInvest_desc",
-    search = "",
-  } = req.query;
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (
+        e.name === "StructError" ||
+        e instanceof Prisma.PrismaClientValidationError
+      ) {
+        res.status(400).send({ message: e.message });
+      } else if (
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === "P2025"
+      ) {
+        res.sendStatus(404);
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  };
+}
 
-  let orderBy;
-  switch (view) {
-    case "accInvest_desc":
-      orderBy = { accInvest: "desc" };
-      break;
-    case "revenue_desc":
-      orderBy = { revenue: "desc" };
-      break;
-    case "employee_desc":
-      orderBy = { employee: "desc" };
-      break;
-    case "employee_asc":
-      orderBy = { employee: "asc" };
-      break;
-    default:
-      orderBy = { id: "asc" };
-      break;
-  }
+app.get(
+  "/companies",
+  asyncHandler(async (req, res) => {
+    const {
+      offset = 0,
+      limit = 10,
+      view = "accInvest_desc",
+      search = "",
+    } = req.query;
 
-  const companies = await prisma.company.findMany({
-    where: search
-      ? {
-          OR: [
-            { name: { contains: search, mode: "insensitive" } },
-            { description: { contains: search, mode: "insensitive" } },
-          ],
-        }
-      : {},
-    skip: parseInt(offset),
-    take: parseInt(limit),
-    orderBy,
-  });
-  res.send(companies);
-});
+    let orderBy;
+    switch (view) {
+      case "accInvest_desc":
+        orderBy = { accInvest: "desc" };
+        break;
+      case "revenue_desc":
+        orderBy = { revenue: "desc" };
+        break;
+      case "employee_desc":
+        orderBy = { employee: "desc" };
+        break;
+      case "employee_asc":
+        orderBy = { employee: "asc" };
+        break;
+      default:
+        orderBy = { id: "asc" };
+        break;
+    }
 
-app.get("/companies/:id", async (req, res) => {
-  const companyId = parseInt(req.params.id);
-  const company = await prisma.company.findUnique({
-    where: { id: companyId },
-  });
-  res.send(company);
-});
+    const companies = await prisma.company.findMany({
+      where: search
+        ? {
+            OR: [
+              { name: { contains: search, mode: "insensitive" } },
+              { description: { contains: search, mode: "insensitive" } },
+            ],
+          }
+        : {},
+      skip: parseInt(offset),
+      take: parseInt(limit),
+      orderBy,
+    });
+    res.send(companies);
+  })
+);
+
+app.get(
+  "/companies/:id",
+  asyncHandler(async (req, res) => {
+    const companyId = parseInt(req.params.id);
+    const company = await prisma.company.findUniqueOrThrow({
+      where: { id: companyId },
+    });
+    res.send(company);
+  })
+);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
