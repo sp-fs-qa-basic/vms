8ef177a66018958e0f7128a7e180a558b3fe5cb3
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-06-11 07:20:46 +0900

    update import

--- FE/src/pages/CompaniesPage.jsx
+++ FE/src/pages/CompaniesPage.jsx
@@ -2,6 +2,7 @@ import MainTable from "@/components/table/mainTable/MainTable";
 import { sortList } from "@/constants/dropdownList";
 import { MainTitleList } from "@/constants/titleList";
 import * as S from "./pages.module.css";
+import MainTableLayout from "@/components/layout/mainTable/MainTableLayout";
 
 function CompaniesPage() {
   return (
