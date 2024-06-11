ed932e7fa22647448446b553bbb8b99cc0b2002f
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-06-12 07:47:56 +0900

    feat : comparePage api add

--- FE/src/pages/ComparisonPage.jsx
+++ FE/src/pages/ComparisonPage.jsx
@@ -1,13 +1,33 @@
+import { getSelect } from "@/api/selection";
 import MainTableLayout from "@/components/layout/mainTable/MainTableLayout";
 import MainTable from "@/components/table/mainTable/MainTable";
-import { sortList } from "@/constants/dropdownList";
-import { MainTitleList } from "@/constants/titleList";
+import { compareSortList } from "@/constants/dropdownList";
+import { CompareTitleList } from "@/constants/titleList";
+import { useEffect, useState } from "react";
 
 function ComparisonPage() {
+  const [companies, setCompanies] = useState([]);
+  const [dropdownValue, setDropdownValue] = useState(compareSortList[0].label);
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      const { view } = compareSortList.find((list) => list["label"] === dropdownValue);
+      const company = await getSelect(view, null, null);
+      console.log(company)
+      setCompanies(company.data);
+    };
+    fetchCompanies();
+  }, [dropdownValue]);
+
   return (
     <>
-      <MainTableLayout title="비교 현황" list={sortList}>
-        <MainTable titles={MainTitleList} />
+      <MainTableLayout
+        title="비교 현황"
+        list={compareSortList}
+        dropdownValue={dropdownValue}
+        setDropdownValue={setDropdownValue}
+      >
+        <MainTable titles={CompareTitleList} lists={companies} />
       </MainTableLayout>
     </>
   );
