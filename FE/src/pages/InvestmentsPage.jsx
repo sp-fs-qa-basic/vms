5d06302d37ca7250df12c245c9749abb5318525c
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-06-17 17:18:01 +0900

    connect : api 연결

--- FE/src/pages/InvestmentsPage.jsx
+++ FE/src/pages/InvestmentsPage.jsx
@@ -4,21 +4,53 @@ import { investSortList } from "@/constants/dropdownList";
 import { InvestTitleList } from "@/constants/titleList";
 import { useEffect, useState } from "react";
 import { getInvestment } from "@/api/investment";
+import { useSearchParams } from "react-router-dom";
 
 function InvestmentsPage() {
   const [companies, setCompanies] = useState([]);
+  const [pagination, setPagination] = useState({});
   const [dropdownValue, setDropdownValue] = useState(investSortList[0].label);
+  const [searchParams, setSearchParams] = useSearchParams({
+    offset: 0,
+    limit: 10,
+    view: "simInvestDesc",
+  });
+
+  const fetchCompanies = async () => {
+    const offset = parseInt(searchParams.get("offset"), 10) || 0;
+    const limit = parseInt(searchParams.get("limit"), 10) || 10;
+    const view = searchParams.get("view") || "revenueDesc";
+
+    const res = await getInvestment(null, view, offset, limit);
+    console.log(res)
+    const extract = res.data.companies.map((company) => ({
+      id: company.companyId,
+      name: company.name,
+      imageUrl: company.imageUrl,
+      description: company.description,
+      category: company.category,
+      actualInvest: company.actualInvest / 10000000,
+      simInvest: company.simInvest / 10000000,
+    }));
+    setCompanies(extract);
+    setPagination(res.data.pagination);
+  };
 
   useEffect(() => {
-    const fetchCompanies = async () => {
-      const { view } = investSortList.find(
-        (list) => list["label"] === dropdownValue
-      );
-      const res = await getInvestment(null, view);
-      setCompanies(res.data.companies);
-    };
+    const { view } = investSortList.find(
+      (list) => list["label"] === dropdownValue
+    );
+
+    setSearchParams((prevParams) => {
+      const params = new URLSearchParams(prevParams);
+      params.set("view", view);
+      params.set("limit", 10);
+      return params;
+    });
+
     fetchCompanies();
-  }, [dropdownValue]);
+  }, [dropdownValue, setSearchParams]);
+
   return (
     <>
       <MainTableLayout
@@ -27,6 +59,10 @@ function InvestmentsPage() {
         dropdownValue={dropdownValue}
         setDropdownValue={setDropdownValue}
         handleRequest={getInvestment}
+        require={false}
+        data={companies}
+        pagination={pagination}
+        onPageChange={setPagination}
       >
         <MainTable titles={InvestTitleList} lists={companies} />
       </MainTableLayout>
