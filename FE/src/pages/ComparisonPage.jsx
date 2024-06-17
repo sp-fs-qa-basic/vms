230c2a0b1cb47319c34f20bfec8443bf2c89309a
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-06-17 16:59:57 +0900

    feat : api연결

--- FE/src/pages/ComparisonPage.jsx
+++ FE/src/pages/ComparisonPage.jsx
@@ -4,19 +4,50 @@ import MainTable from "@/components/table/mainTable/MainTable";
 import { compareSortList } from "@/constants/dropdownList";
 import { CompareTitleList } from "@/constants/titleList";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function ComparisonPage() {
   const [companies, setCompanies] = useState([]);
+  const [pagination, setPagination] = useState({});
   const [dropdownValue, setDropdownValue] = useState(compareSortList[0].label);
+  const [searchParams, setSearchParams] = useSearchParams({
+    offset: 0,
+    limit: 10,
+    view: "mySelectionDesc",
+  });
+
+  const fetchCompanies = async () => {
+    const offset = parseInt(searchParams.get("offset"), 10) || 0;
+    const limit = parseInt(searchParams.get("limit"), 10) || 10;
+    const view = searchParams.get("view") || "revenueDesc";
+
+    const res = await getSelect(view, offset, limit);
+
+    const extract = res.data.companies.map((company) => ({
+      id: company.companyId,
+      name: company.name,
+      imageUrl: company.imageUrl,
+      description: company.description,
+      category: company.category,
+      mySelectionCount: company.mySelectionCount,
+      comparedSelectionCount: company.comparedSelectionCount
+    }));
+    setCompanies(extract);
+    setPagination(res.data.pagination);
+  };
 
   useEffect(() => {
-    const fetchCompanies = async () => {
-      const { view } = compareSortList.find((list) => list["label"] === dropdownValue);
-      const res = await getSelect(view, null, null);
-      setCompanies(res.data.companies);
-    };
+    const { view } = compareSortList.find((list) => list["label"] === dropdownValue);
+
+    setSearchParams((prevParams) => {
+      const params = new URLSearchParams(prevParams);
+
+      params.set("view", view);
+      params.set("limit", 10);
+      return params;
+    });
     fetchCompanies();
-  }, [dropdownValue]);
+  }, [dropdownValue, setSearchParams]);
 
   return (
     <>
@@ -26,6 +57,10 @@ function ComparisonPage() {
         dropdownValue={dropdownValue}
         setDropdownValue={setDropdownValue}
         handleRequest={getSelect}
+        data={companies}
+        require={false}
+        pagination={pagination}
+        onPageChange={setPagination}
       >
         <MainTable titles={CompareTitleList} lists={companies} />
       </MainTableLayout>
